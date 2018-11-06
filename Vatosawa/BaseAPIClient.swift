9e7644d89c4c81f3005a37d3976063c25c19606a
Genesis <gsanguino@condorlabs.io>
2018-11-06 17:26:43 -0500

    Modified type cases

--- Vatosawa/BaseAPIClient.swift
+++ Vatosawa/BaseAPIClient.swift
@@ -25,6 +25,12 @@ public enum APIAuthorizationType: String {
 public enum APIContentType: String {
     case json = "application/json"
     case formURLencoded = "application/x-www-form-urlencoded"
+    case jpeg = "image/jpeg"
+    case png = "image/png"
+    case anyImage = "image/*"
+    case pdf = "application/pdf"
+    case anyDocument = "application/*"
+    case any = "*/*"
 }
 
 public class BaseAPIClient: ClientProtocol {
